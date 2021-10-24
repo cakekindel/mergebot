a2f441a70cfc6fe663dacecca8c6548b6e85344e
mergebot <donotreply@mergebot.orionkindel.com>
2021-10-24 13:12:08 -0700

    fix tests

--- tests/slack.rs
+++ tests/slack.rs
@@ -10,6 +10,7 @@ fn mk_api(client: &'static Client) -> slack::Api {
   slack::Api::new(mockito::server_url(), "xoxb", client)
 }
 
+#[test]
 pub fn groups() {
   use slack::groups::Groups;
 
@@ -21,8 +22,8 @@ pub fn groups() {
     ]
   });
 
-  let _ = mock("GET", "/api/usergroups.users.list?usergroup=ABC123").match_header("authorization",
-                                                                                  Match::Exact("xoxb".into()))
+  let moq = mock("GET", "/api/usergroups.users.list?usergroup=ABC123").match_header("authorization",
+                                                                                  Match::Exact("Bearer xoxb".into()))
                                                                     .with_status(200)
                                                                     .with_header("Content-Type", "application/json")
                                                                     .with_body(serde_json::to_string(&rep).unwrap())
@@ -34,10 +35,12 @@ pub fn groups() {
 
   let res = api.expand("ABC123");
 
-  assert!(res.is_ok());
+  moq.assert();
+
   assert_eq!(res.unwrap(), vec!["user_a".to_string(), "user_b".to_string()])
 }
 
+#[test]
 pub fn messages_send() {
   use slack::msg::Messages;
 
@@ -48,7 +51,7 @@ pub fn messages_send() {
 
   let rep = serde_json::json!({
     "ok": true,
-    "channel": "C1H9RESGL",
+    "channel": "C1234",
     "ts": "1503435956.000247",
     "message": {
       "text": "Here's a message for you",
@@ -67,7 +70,7 @@ pub fn messages_send() {
     }
   });
 
-  let _ = mock("POST", "/api/chat.postMessage").match_header("authorization", Match::Exact("xoxb".into()))
+  let moq = mock("POST", "/api/chat.postMessage").match_header("authorization", Match::Exact("Bearer xoxb".into()))
                                                .match_body(Match::Json(body_expected))
                                                .with_status(200)
                                                .with_header("Content-Type", "application/json")
@@ -80,11 +83,13 @@ pub fn messages_send() {
 
   let res = api.send("C1234", &[]);
 
-  assert!(res.is_ok());
+  moq.assert();
+
   assert_eq!(res.unwrap().id,
              slack::msg::Id { ts: "1503435956.000247".to_string(),
                               channel: "C1234".to_string() })
 }
+#[test]
 pub fn messages_send_thread() {
   use slack::msg::Messages;
 
@@ -96,7 +101,7 @@ pub fn messages_send_thread() {
 
   let rep = serde_json::json!({
     "ok": true,
-    "channel": "C1H9RESGL",
+    "channel": "C1234",
     "ts": "1503435956.000247",
     "message": {
       "text": "Here's a message for you",
@@ -115,7 +120,7 @@ pub fn messages_send_thread() {
     }
   });
 
-  let _ = mock("POST", "/api/chat.postMessage").match_header("authorization", Match::Exact("xoxb".into()))
+  let moq = mock("POST", "/api/chat.postMessage").match_header("authorization", Match::Exact("Bearer xoxb".into()))
                                                .match_body(Match::Json(body_expected))
                                                .with_status(200)
                                                .with_header("Content-Type", "application/json")
@@ -130,8 +135,19 @@ pub fn messages_send_thread() {
                                               channel: "C1234".to_string() },
                             &[]);
 
-  assert!(res.is_ok());
+  moq.assert();
+
   assert_eq!(res.unwrap().id,
              slack::msg::Id { ts: "1503435956.000247".to_string(),
                               channel: "C1234".to_string() })
+}
+
+#[test]
+pub fn authentic() {
+  // from slack examples
+  let signing_secret = "8f742231b10e8888abcd99yyyzzz85a5";
+  let body = "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c";
+  let timestamp = "1531420618";
+  let inbound_sig = "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503";
+  assert!(mergebot::slack::request_authentic(signing_secret, bytes::Bytes::from(body), http::HeaderValue::from_str(timestamp).unwrap(), http::HeaderValue::from_str(inbound_sig).unwrap()));
 }
