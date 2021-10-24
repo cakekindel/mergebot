2e3879e76dc61266834e67031b1476ddba417bf2
Orion Kindel <cakekindel@gmail.com>
2021-10-24 13:36:47 -0700

    fmt

--- src/git/impl/client.rs
+++ src/git/impl/client.rs
@@ -79,7 +79,10 @@ impl git::Client for StaticClient {
     let git = lock.as_ref().unwrap();
 
     git.git(&["config", "--get", "user.email"])
-       .and_then_err(|e| match e {Error::CommandFailed(out) => Ok(out), _ => Err(e)})
+       .and_then_err(|e| match e {
+         | Error::CommandFailed(out) => Ok(out),
+         | _ => Err(e),
+       })
        .and_then(|out| {
          if out.0.is_empty() {
            git.git(&["config",
@@ -92,7 +95,12 @@ impl git::Client for StaticClient {
            Ok(())
          }
        })
-       .and_then(|_| git.git(&["config", "--get", "user.name"]).and_then_err(|e| match e {Error::CommandFailed(out) => Ok(out), _ => Err(e)}))
+       .and_then(|_| {
+         git.git(&["config", "--get", "user.name"]).and_then_err(|e| match e {
+                                                     | Error::CommandFailed(out) => Ok(out),
+                                                     | _ => Err(e),
+                                                   })
+       })
        .and_then(|out| {
          if out.0.is_empty() {
            git.git(&["config", "--global", "user.name", "mergebot"]).map(|_| ())
