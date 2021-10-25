03a0e1ad90a010170753b28d8844e2daf567ebfc
Orion Kindel <cakekindel@gmail.com>
2021-10-25 11:50:30 -0700

    test: misc unit tests

--- src/extra/str.rs
+++ src/extra/str.rs
@@ -8,3 +8,16 @@ impl<T: AsRef<str>> StrExtra for T {
     self.as_ref().trim().to_lowercase() == other.as_ref().trim().to_lowercase()
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn loose_eq() {
+    let a = "    foo ";
+    let b = " FoO         \n";
+
+    assert!(a.loose_eq(b));
+  }
+}
